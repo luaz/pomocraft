d9670d1226bcbaf124413301a8a52a8ae725895c
luaz <desmond.lua@gmail.com>
2024-05-13 23:03:15 +0800

    fix timer width to avoid ui flow

--- components/LxTimer.vue
+++ components/LxTimer.vue
@@ -25,7 +25,7 @@ const modeText = computed(() => {
   if (state.mode == Mode.RUNNING) return 'Focus'
   else if (state.mode == Mode.COMPLETED) return 'Take a break'
   else if (state.mode == Mode.RESTING) return 'Taking a break'
-  else if (state.mode == Mode.IDLE) return "Let's start"
+  else if (state.mode == Mode.IDLE) return "Ready"
 })
 
 function changeMode() {
@@ -127,7 +127,7 @@ function blinkTitle(title, filler) {
       clearInterval(blinkTimerId)
       blinkTimerId = null
     }
-  }, 500); // Blink interval in milliseconds (e.g., 500ms for half-second blink)
+  }, 600); // Blink interval in milliseconds (e.g., 500ms for half-second blink)
 }
 
 watch(() => state.timerText, (timerText) => { 
@@ -143,7 +143,7 @@ watch(() => state.timerText, (timerText) => {
   <div class="flex w-48 py-4 px-4 rounded-md" :class="{ 'bg-slate-100': state.mode == Mode.IDLE, 'bg-lime-100': state.mode == Mode.RUNNING, 'bg-red-100': state.mode == Mode.COMPLETED, 'bg-orange-100': state.mode == Mode.RESTING }">
     <div class="flex-1 justify-center">
       <div class="text-center text-sm text-slate-500">{{ modeText }}</div>
-      <div class="text-center text-5xl font-medium dark:text-slate-500" :class="{ 'animate-blink': state.mode == Mode.COMPLETED}">{{ state.timerText }}</div>
+      <div class="text-center text-5xl font-medium dark:text-slate-500 w-32" :class="{ 'animate-blink': state.mode == Mode.COMPLETED}">{{ state.timerText }}</div>
       <div class="text-center text-sm text-slate-700 font-medium text-ellipsis ">
         {{ taskName || 'Select a Task' }}
       </div>
