d79ba8bff675fdfbb59b049d517dc564b93ab93c
d_luaz <desmond.lua@gmail.com>
2026-02-12 00:46:59 +0800

    fix tailwind opacity issue

--- README.md
+++ README.md
@@ -13,8 +13,13 @@ npm run dev
 
 ## TODO
 
+- Delete/Complete Task
 - Export IndexedDB: https://dexie.org/docs/ExportImport/dexie-export-import
 - Task add description/note
 - Highlght active task
 - Swap task half way
 - Show average pomodoro per day for the last 30 days
+
+## UPDATES
+
+2026-02-12: Delete/Undelete Project
--- components/LxFocus.vue
+++ components/LxFocus.vue
@@ -18,20 +18,20 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['closed', 'completed'])
+const emit = defineEmits(["closed", "completed"]);
 
 // const progress = ref(0)
 const animPosition = computed(() => {
-  return (200 * (props.progress))
-})
+  return 200 * props.progress;
+});
 const barColor = computed(() => {
-  if (props.progress <= 0.25) return 'bg-purple-300';
-  if (props.progress <= 0.50) return 'bg-blue-500';
-  if (props.progress <= 0.75) return 'bg-yellow-500';
-  return 'bg-green-500'; // Gold-ish
+  if (props.progress <= 0.25) return "bg-purple-300";
+  if (props.progress <= 0.5) return "bg-blue-500";
+  if (props.progress <= 0.75) return "bg-yellow-500";
+  return "bg-green-500"; // Gold-ish
 });
 
-const bgImgSrc = ref(`/img/motiv/${genRandomImageId(15)}.jpg`)
+const bgImgSrc = ref(`/img/motiv/${genRandomImageId(15)}.jpg`);
 
 function genRandomImageId(total) {
   return Math.floor(Math.random() * total) + 1;
@@ -41,10 +41,10 @@ watch(
   () => props.progress,
   (newProgress) => {
     if (newProgress == 1) {
-      bgImgSrc.value = `/img/done/${genRandomImageId(13)}.jpg`
+      bgImgSrc.value = `/img/done/${genRandomImageId(13)}.jpg`;
     }
-  }
-)
+  },
+);
 
 /*
 watch(
@@ -79,16 +79,18 @@ onBeforeUnmount(() => {
  */
 </script>
 
-
 <template>
   <div v-bind="$attrs" class="fixed inset-0 bg-black z-[1000]">
-
     <button
       @click="$emit('closed')"
       class="absolute top-1.5 right-1.5 text-gray-400 hover:text-white transition-colors duration-200 rounded-full hover:bg-white/10 p-0 z-[1001]"
       aria-label="Close"
     >
-      <UIcon name="i-heroicons-x-mark" class="size-10" @click="emit('closed')"/>
+      <UIcon
+        name="i-heroicons-x-mark"
+        class="size-10"
+        @click="emit('closed')"
+      />
     </button>
 
     <!--
@@ -106,11 +108,12 @@ onBeforeUnmount(() => {
       <div>
         <div class="relative">
           <transition name="fade" mode="out-in">
-            <img 
-            :key="bgImgSrc"
-            :src="bgImgSrc" 
-            alt="Background" 
-            class="w-full h-full object-cover" />
+            <img
+              :key="bgImgSrc"
+              :src="bgImgSrc"
+              alt="Background"
+              class="w-full h-full object-cover"
+            />
           </transition>
 
           <!--
@@ -119,7 +122,6 @@ onBeforeUnmount(() => {
             <div class="w-24 bg-gradient-to-l from-black to-transparent"></div>
           </div>
            -->
-
 
           <!-- Foreground text -->
 
@@ -134,17 +136,18 @@ onBeforeUnmount(() => {
                 />
               </div>
 
-
               <div class="bg-gray-200 rounded-full h-6 overflow-hidden">
                 <div
                   class="rounded-full h-6 transition-all duration-500"
                   :class="barColor"
-                  :style="{ width: (progress * 100) + '%' }"
+                  :style="{ width: progress * 100 + '%' }"
                 ></div>
               </div>
             </div>
 
-            <div class="text-white text-8xl font-bold bg-black bg-opacity-30 px-4 pt-0 pb-4 m-0 rounded-xl min-w-80 text-center">
+            <div
+              class="text-white text-8xl font-bold bg-black/30 px-4 pt-0 pb-4 m-0 rounded-xl min-w-80 text-center"
+            >
               {{ timerText || "00:00" }}
             </div>
 
@@ -155,27 +158,13 @@ onBeforeUnmount(() => {
             <div class="flex justify-center mt-5" v-if="progress == 1">
               <button
                 @click="emit('completed')"
-                class="
-                  bg-gradient-to-r from-green-400 to-blue-500
-                  text-white
-                  px-8 py-4
-                  rounded-full
-                  shadow-lg
-                  hover:shadow-xl
-                  font-bold text-2xl
-                  flex items-center gap-3
-                  border-2 border-white/20
-                  transition-all duration-300
-                  hover:scale-105
-                  focus:outline-none focus:ring-4 focus:ring-green-300/50
-                "
+                class="bg-gradient-to-r from-green-400 to-blue-500 text-white px-8 py-4 rounded-full shadow-lg hover:shadow-xl font-bold text-2xl flex items-center gap-3 border-2 border-white/20 transition-all duration-300 hover:scale-105 focus:outline-none focus:ring-4 focus:ring-green-300/50"
               >
                 <UIcon name="i-heroicons-check-circle" class="size-8" />
                 Good Job!
               </button>
             </div>
           </div>
-
         </div>
       </div>
     </div>
